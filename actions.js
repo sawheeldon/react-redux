a48619f61294505fdd40db37d609d346f8804daa
Simon <simon@jampot.design>
2016-09-26 23:16:48 +0000

    added async actions

--- actions.js
+++ actions.js
@@ -1,3 +1,5 @@
+require('isomorphic-fetch');
+
 var ADD_REPOSITORY = 'ADD_REPOSITORY';
 var addRepository = function(repository) {
     return {
@@ -14,6 +16,59 @@ var rateRepository = function(repository, rating) {
         rating: rating
     };
 };
+
+var FETCH_DESCRIPTION_SUCCESS = 'FETCH_DESCRIPTION_SUCCESS';
+var fetchDescriptionSuccess = function(repository, description) {
+    return {
+        type: FETCH_DESCRIPTION_SUCCESS,
+        repository: repository,
+        description: description
+    };
+};
+
+var FETCH_DESCRIPTION_ERROR= 'FETCH_DESCRIPTION_ERROR';
+var fetchDescriptionError = function(repository, error) {
+    return {
+        type: FETCH_DESCRIPTION_ERROR,
+        repository: repository,
+        error: error
+    };
+};
+
+var fetchDescription = function(repository) {
+    return function(dispatch) {
+        var url = 'https://api.github.com/repos/' + repository;
+        return fetch(url).then(function(response) {
+            if (response.status < 200 || response.status >= 300) {
+                var error = new Error(response.statusText)
+                error.response = response
+                throw error;
+            }
+            return response;
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var description = data.description;
+            return dispatch(
+                fetchDescriptionSuccess(repository, description)
+            );
+        })
+        .catch(function(error) {
+            return dispatch(
+                fetchDescriptionError(repository, error)
+            );
+        });
+    }
+};
+
+exports.fetchDescription = fetchDescription;
+
+exports.FETCH_DESCRIPTION_SUCCESS = FETCH_DESCRIPTION_SUCCESS;
+exports.fetchDescriptionSuccess = fetchDescriptionSuccess;
+exports.FETCH_DESCRIPTION_ERROR = FETCH_DESCRIPTION_ERROR;
+exports.fetchDescriptionError = fetchDescriptionError;
 
 exports.ADD_REPOSITORY = ADD_REPOSITORY;
 exports.addRepository = addRepository;
--- reducers.js
+++ reducers.js
@@ -10,7 +10,7 @@ var repositoryReducer = function(state, action) {
             rating: null
         });
     }
-    else if (action.type === actions.RATE_REPOSITORY) {
+     else if (action.type === actions.FETCH_DESCRIPTION_SUCCESS) {
         // Find the index of the matching repository
         var index = -1;
         for (var i=0; i<state.length; i++) {
@@ -20,6 +20,30 @@ var repositoryReducer = function(state, action) {
                 break;
             }
         }
+
+        if (index === -1) {
+            throw new Error('Could not find repository');
+        }
+
+        var before = state.slice(0, i);
+        var after = state.slice(i + 1);
+        var newRepository = Object.assign({}, repository, {
+            description: action.description
+        });
+        return before.concat(newRepository, after);
+    }
+    else if (action.type === actions.FETCH_DESCRIPTION_ERROR) {
+        // Find the index of the matching repository
+        var index = -1;
+        for (var i=0; i<state.length; i++) {
+            var repository = state[i];
+            if (repository.name === action.repository) {
+                index = i;
+                break;
+            }
+        }
+        
+        
 
         if (index === -1) {
             throw new Error('Could not find repository');
